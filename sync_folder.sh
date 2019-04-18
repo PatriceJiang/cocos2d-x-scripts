78ae9079fceb3efaf62ca7afea233d80908ede79
Arnold <397136899@qq.com>
2019-04-18 11:45:35 +0800

    ss

---
+++ sync_folder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+src_folder=/mnt/e/Github/cocos2d-x/tests/lua-tests/src
+dst_folder=/mnt/e/Projects/MetalTrans/bin/lua-tests/Debug/Resources/src
+
+while inotifywait -r $src_folder/*; do 
+	rsync -rvu $src_folder/* $dst_folder
+done
