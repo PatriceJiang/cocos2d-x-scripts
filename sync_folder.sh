e5dcaaee3966058f5ca5187ec38ab8aad406fd89
Arnold <397136899@qq.com>
2019-04-24 16:00:03 +0800

    save

--- sync_folder.sh
+++ sync_folder.sh
@@ -1,8 +1,20 @@
 #!/bin/bash 
 
-src_folder=/mnt/e/Github/cocos2d-x/tests/lua-tests/src
-dst_folder=/mnt/e/Projects/MetalTrans/bin/lua-tests/Debug/Resources/src
+unamestr=`uname`
+root="/mnt"
 
+if [[ $unamestr =~ .*CYGWIN.* ]]; then
+	echo "using cygwin"
+	root="/cygdrive"
+fi
+
+
+src_folder=${root}/e/Github/cocos2d-x/tests/lua-tests/src
+dst_folder=${root}/e/Projects/MetalTrans/bin/lua-tests/Debug/Resources/src
+
+echo "copy files ... "
+rsync -rvu $src_folder/* $dst_folder
+echo "watching files ..."
 while inotifywait -r $src_folder/*; do 
 	rsync -rvu $src_folder/* $dst_folder
 done
