072d0d824f29e4bdbbe70bed109d447eb891fe17
PatriceJiang <397136899@qq.com>
2020-04-08 18:59:43 +0800

    update gfx

--- 3d_update_gfx.sh
+++ 3d_update_gfx.sh
@@ -6,8 +6,6 @@ dir1=cocos2d-x-lite
 dir2=engine
 dir3=jsb-adapter
 
-#dir1_branch=3fgfx-win32-adaptation
-#common_branch=3d-gfx
 
 
 if [ $# -lt 1 ]; then
@@ -20,7 +18,6 @@ echo "argument branch is not set"
 exit -1
 fi
 
-#dir1_branch=$1
 common_branch=$1
 
 
@@ -48,18 +45,24 @@ echo "[$dir1] on $branch1"
 echo "[$dir2] on $branch2"
 echo "[$dir3] on $branch3"
 
+echo " -> set branch to $common_branch"
+
+if [ $branch1 != $common_branch ]; then
+    echo " => [$dir1] should be on ${common_branch}"
+    git -C $dir1 stash push
+    git -C $dir1 checkout $common_branch
+fi
+
 if [ $branch2 != $common_branch ]; then
-    echo " [$dir2] should be on ${common_branch}"
+    echo " => [$dir2] should be on ${common_branch}"
     git -C $dir2 stash push
     git -C $dir2 checkout $common_branch
-   # exit -1
 fi
 
 if [ $branch3 != $common_branch ]; then 
-    echo " [$dir3] should be on ${common_branch}"
+    echo " => [$dir3] should be on ${common_branch}"
     git -C $dir3 stash push
     git -C $dir3 checkout $common_branch
-   # exit -1
 fi
 
 
